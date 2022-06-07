f80ce30b1cd2295a9b962f0c8f953885843d92c4
takumi <tkmyy0201@gmail.com>
2022-06-07 19:23:24 +0900

    create template for top page

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,20 @@
 import Head from "next/head";
-import Image from 'next/image';
+// import Image from 'next/image';
+import styles from "./layout.module.css"
+import utilStyles from "../styles/utils.module.css"
 
 const name = "Shin code"
 export const siteTitle = "Next.js blog"
 
 export default function Layout({children}) {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <header>
-                <Image
-                src="/images/profile.png"
-                alt="profile"
-                width={50}
-                height={50}
-                />
-                <h1>{name}</h1>
+            <header className={styles.header}>
+                <img src="/images/profile.png" className={utilStyles.borderCircle}/>
+                <h1 className={utilStyles.heading2Xl}>{name}</h1>
             </header>
             <main>{children}</main>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,87 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import Layout from '../components/Layout';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
 
 import Link from "next/link";
+import utilStyle from "../styles/utils.module.css";
 
 export default function Home() {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <section className={utilStyle.headingMd}>
+        <p>nextjs</p>
+      </section>
+      <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
+        <h2>🗒エンジニアのブログ</h2>
+
+        <div className={styles.grid}>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+                alt=""
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+                alt=""
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+                alt=""
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2020</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+                alt=""
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2020</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
